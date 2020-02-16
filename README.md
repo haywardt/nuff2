1bafb27a50c35268f27a28afb455a9ee51c26a5a
Glitch (good-nuff2) <none>
2020-02-16 22:32:53 +0000

    🕍🕰 working with templates. now needs contenteditable

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
+# HELPERS!
+Use the area below as a chat window. Most recent comments at the top.
+The broken stuff is in a file called `broken.js` so you don't have to read
+the rest of my code unless you want to.
+
+--------------------------------------------------------------------
+
+you:  
+Tim: Thanks for offering to help!  
+
+---------------------------------------------------------------------
+
+
+
+# To Do
+- ~~update innerText rather than innerHTML on content objects.~~
+- ignore proxy calls when changes dont occur
+- ignore `blur` when content doesn't change.
+- deal with buttons and anchors
+- decide if we should work with `contenteditable` since it doesn't fire a `change` event. Could use `input`
+  - add `blur` to all content editable at startup and to cloned `template` elements
+- ~~figure out repeating elements~~
+- create a state.get that retrieves arraylike keys into an array
+- figure out where `state` and `api` are glued together.
+
+
+# Good-Nuff
+
+- When any element on the front end changes it is reflected on the backend in Object `state`
+- When any property of Object `state` changes on the back end it is reflected on the frontend elements
+- Things in Object `state` are private to this cookieholder
+- Things in Object `api` are shared amoung all those with the same value of `state.groupSecret`
+- **Warning**: array objects in `state` are not really arrays, but are top level properties written 
+in the form of an array(ie:`state["cart[0].qty"]` rather than `state.cart[0].qty`. You must convert them to arrays 
+before trying to iterate over them. You cannot set the properties of an array element with a {} but must 
+set each one individually.
+
+-- Tim Hayward
+
 # Basic PWA Page
 
 Probably the simplest progressive web app, just to get you started.
@@ -9,12 +48,3 @@ It
 3. Is just this.
 
 -- Paul Kinlan
-
-# Good-Nuff
-
-- When any element on the front end changes it is reflected on the backend in Object `state`
-- When any property of Object `state` changes on the back end it is reflected on the frontend elements
-- Things in Object `state` are private to this cookieholder
-- Things in Object `api` are shared amoung all those with the same value of `state.groupSecret`
-
--- Tim Hayward
--- nuff.js
+++ nuff.js
@@ -5,24 +5,53 @@ const stateHandler = {
     serverChanged(obj, prop, value);
   }
 };
+
 function clientChanged() {
   console.log("client changed");
-  if(event.target.type=="checkbox" && event.target.checked==false){
-    state[event.target.name]=null;}
-  else {
+  if (event.target.type == "checkbox" && event.target.checked == false) {
+    state[event.target.name] = null;
+  } else {
     state[event.target.name] = event.target.value;
   }
 }
 function serverChanged(obj, prop, value) {
   console.log("server changed");
   obj[prop] = value;
-  
+  // check to see if this data needs to create a new element because an array item has grown
+
+  var propertyIsArray = /(([-_\w\d\s]+)\[(\d)\].)\w*/;
+  var match = prop.match(propertyIsArray); // see if this property is formatted like an array
+  if (match) {
+    // check to see if any existing DOM elements match this array index, and if not clone a new one from the template.
+    let elem = document.querySelector(`[name^="${match[1]}"]:not(template)`);
+    if (!elem) {
+      // this prop[index] does not exist so clone it from the template
+      // Get the last <li> element ("Milk") of <ul> with id="myList2"
+
+      let temp = document.querySelector(`template[name="${match[2]}"]`);
+      let elemPosition = temp.parentElement;
+
+      // Copy the template element and its child nodes
+      var cln = temp.content.firstElementChild.cloneNode(true);
+      //put the index in the name
+      let arrayRef = new RegExp(match[2] + "\\[\\]\\.", "g");
+      cln.innerHTML = cln.innerHTML.replace(arrayRef, match[1]);
+      // Append the template element clone to end of the template container
+      elemPosition.appendChild(cln);
+
+      // Now change all the [] in the element names of the cloned element to [index]
+    }
+  }
+
+  // peruse the document to see what elements need this new data
+    // handle things that need their value updated
   var dataFields = document.querySelectorAll(
     'input[name="' + prop + '"]:not([type=radio]):not([type=checkbox])'
   );
   for (let field of dataFields) {
     field.value = value;
   }
+  // handle things that need their checked property updated
   var uncheckRadios = document.querySelectorAll(
     'input[name="' + prop + '"][type=radio]'
   );
@@ -49,15 +78,18 @@ function serverChanged(obj, prop, value) {
   for (let radio of checkRadios) {
     radio.checked = "true";
   }
+  
+  // update things that need their innerText updated. (innerHTML might be a security risk)
   var htmlContent = document.querySelectorAll(
     '[name="' + prop + '"]:not(input):not(button)'
   );
   for (let content of htmlContent) {
-    content.innerHTML = value;
+    content.innerText = value;
   }
   return true;
 }
 var state = new Proxy(hiddenState, stateHandler);
+
 document.addEventListener("DOMContentLoaded", function() {
   document.querySelector("body").addEventListener("change", clientChanged);
 });
